665089016242b3ac2e5ee774a1823db63c55fcbf
danimohli <danimohlife@gmail.com>
2024-09-30 19:05:22 -0400

    blockscop

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;   // Using const since the value is not meant to change
-  const task2 = true;   // Using const here as well for the same reason
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;    // let ensures this is scoped within the if block only
-    let task2 = false;  // let ensures this is scoped within the if block only
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
