d02c177b265a233173fe031f0af7fb1904d9595f
danimohli <danimohlife@gmail.com>
2024-10-03 10:03:20 -0400

    build

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,28 @@
 class Building {
-	constructor(sqft) {
-		if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-			throw new Error('Class extending Building must override evacuationWarningMessage');
-		}
-		this._sqft = sqft;
-	}
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
 
-	/**
-	 * Getter for sqft attribute
-	 * @returns {Number} sqft
-	 */
-	get sqft() {
-		return this._sqft;
-	}
+  /**
+   * @param {Number} sqft
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
 
 export default Building;
