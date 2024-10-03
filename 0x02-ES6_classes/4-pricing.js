ace4891eb25b54503ff5ea8e151f5c8f19091485
danimohli <danimohlife@gmail.com>
2024-10-03 09:37:31 -0400

    price

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,57 +1,57 @@
 import Currency from './3-currency.js';
 
 export default class Pricing {
-  constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
-  }
+	constructor(amount, currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 
-  /**
-   * @param {Number} amount
-   */
-  set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = amount;
-  }
+	/**
+	 * @param {Number} amount
+	 */
+	set amount(amount) {
+		if (typeof amount !== 'number') {
+			throw new TypeError('Amount must be a number');
+		}
+		this._amount = amount;
+	}
 
-  get amount() {
-    return this._amount;
-  }
+	get amount() {
+		return this._amount;
+	}
 
-  /**
-   * @param {Currency} currency
-   */
-  set currency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of the Currency class');
-    }
-    this._currency = currency;
-  }
+	/**
+	 * @param {Currency} currency
+	 */
+	set currency(currency) {
+		if (!(currency instanceof Currency)) {
+			throw new TypeError('Currency must be an instance of the Currency class');
+		}
+		this._currency = currency;
+	}
 
-  get currency() {
-    return this._currency;
-  }
+	get currency() {
+		return this._currency;
+	}
 
-  /**
-   * Returns a string in the format "amount currency_name (currency_code)"
-   * @returns {String}
-   */
-  displayFullPrice() {
-    return `${this.amount} ${this.currency.displayFullCurrency()}`;
-  }
+	/**
+	 * Returns a string in the format "amount currency_name (currency_code)"
+	 * @returns {String}
+	 */
+	displayFullPrice() {
+		return `${this.amount} ${this.currency.displayFullCurrency()}`;
+	}
 
-  /**
-   * Converts the price based on a given conversion rate.
-   * @param {Number} amount
-   * @param {Number} conversionRate
-   * @returns {Number}
-   */
-  static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Amount and conversion rate must be numbers');
-    }
-    return amount * conversionRate;
-  }
+	/**
+	 * Converts the price based on a given conversion rate.
+	 * @param {Number} amount
+	 * @param {Number} conversionRate
+	 * @returns {Number}
+	 */
+	static convertPrice(amount, conversionRate) {
+		if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+			throw new TypeError('Amount and conversion rate must be numbers');
+		}
+		return amount * conversionRate;
+	}
 }
