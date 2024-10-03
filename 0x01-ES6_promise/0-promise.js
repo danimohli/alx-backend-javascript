113fb3af483e96124cca144ab0f37981df03f8f2
danimohli <danimohlife@gmail.com>
2024-10-03 05:40:45 -0400

    every promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate API call success or failure
-    const success = true; // Change to false to simulate failure
-
-    if (success) {
-      resolve("Success!"); // Promise resolved
-    } else {
-      reject("Failed!");   // Promise rejected
-    }
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
