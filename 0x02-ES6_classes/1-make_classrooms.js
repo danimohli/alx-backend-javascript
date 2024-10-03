8d901967c855d81a9dfd59df0bb2f0409f75b9c5
danimohli <danimohlife@gmail.com>
2024-10-03 07:29:50 -0400

    comment

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,6 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  // Return an array with 3 ClassRoom objects
   return [
     new ClassRoom(19),
     new ClassRoom(20),
