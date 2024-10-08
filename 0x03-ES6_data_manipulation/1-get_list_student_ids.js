4daa0e5c5886f740423839fe8732d2c39ca8d671
danimohli <danimohlife@gmail.com>
2024-10-08 10:25:13 +0100

    student_ids

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
-function getListStudentIds(students) {
+function getListStudentIds (students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  
+
   return students.map(student => student.id);
 }
 
-export default getListStudentIds;
+module.exports = getListStudentIds;
