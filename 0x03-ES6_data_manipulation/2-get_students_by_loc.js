d7c6f029c4b076cf9a12f98375610f8b40749eaa
danimohli <danimohlife@gmail.com>
2024-10-08 12:56:42 +0100

    by loc

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+function getStudentsByLocation(students, city) {
+    if (!Array.isArray(students) || typeof city !== 'string') {
+        return [];
+    }
+    return students.filter(student => student.location === city);
+  }
+  module.exports = getStudentsByLocation;
