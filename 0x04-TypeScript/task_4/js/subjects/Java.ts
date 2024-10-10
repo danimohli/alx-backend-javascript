32958a3685ef6cb1c733230a904b43ee84f02e2e
danimohli <danimohlife@gmail.com>
2024-10-10 04:40:32 +0100

    task_4/

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,22 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingJava?: number; // Declaration merging
+    }
+
+    export class Java extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for Java';
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            } else {
+                return 'No available teacher';
+            }
+        }
+    }
+    export const java = new Java();
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,22 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingReact?: number; // Declaration merging
+    }
+
+    export class React extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for React';
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            } else {
+                return 'No available teacher';
+            }
+        }
+    }
+    export const react = new React();
+}
