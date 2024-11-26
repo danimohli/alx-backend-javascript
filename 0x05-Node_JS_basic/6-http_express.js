bf0d3e389ecbd49b7c66ef38b7f697c2b232ac77
danimohli <danimohlife@gmail.com>
2024-11-27 00:14:23 +0100

    xpres

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
 const express = require('express');
-const app = express();
 
-// Root route to display "Hello Holberton School!"
+const app = express();
+const PORT = 1245;
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Set the server to listen on port 1245
-const PORT = 1245;
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-// Export the app for testing or use in other modules
 module.exports = app;
