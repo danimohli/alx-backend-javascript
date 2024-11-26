d6bcd7e9d3dc22ae599be2cecad7b283bf2b8170
danimohli <danimohlife@gmail.com>
2024-11-26 21:21:41 +0100

    msg

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Function that prints a string to STDOUT.
+ * @param {string} message - The message to display.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
