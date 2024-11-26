82ae519875235ace34f996151289d50991706239
danimohli <danimohlife@gmail.com>
2024-11-26 21:39:25 +0100

    std

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 
-// Display initial message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim(); // Get user input, trimmed of whitespace
-  console.log(`Your name is: ${input}`);
-
-  // Exit process
-  console.log('This important software is now closing');
-  process.exit();
-});
-
-// Handle program exit explicitly
-process.on('SIGINT', () => {
-  console.log('\nThis important software is now closing');
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('SIGNIT', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
